67e623fdc016e492257abad3717f04394e0c8871
Jimin Park <pjm950@gmail.com>
2025-07-13 01:08:31 +0900

    Implemented config reading.

---
+++ src/config.rs
@@ -0,0 +1,29 @@
+use serde::Deserialize;
+use std::{fs, path::Path};
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    #[serde(default)]
+    pub deploy: Vec<Deploy>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Deploy {
+    pub repository: String,
+    #[serde(default)]
+    pub artifact: Vec<Artifact>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Artifact {
+    pub name: String,
+    pub target: String,
+}
+
+impl Config {
+    pub fn from_file<P: AsRef<Path>>(path: P) -> Result<Self, Box<dyn std::error::Error>> {
+        let data = fs::read_to_string(path)?;
+        let config = toml::from_str(&data)?;
+        Ok(config)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-#![deny(warnings)]
+// #![deny(warnings)]
 
+use clap::Parser;
 use warp::Filter;
 
+mod config;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(long)]
+    config: String,
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    let _config = config::Config::from_file(&args.config)?;
+
     let port: u16 = std::env::var("PORT")
         .ok()
         .and_then(|p| p.parse().ok())
@@ -15,4 +27,6 @@ async fn main() {
 
     println!("Listening on 0.0.0.0:{}", port);
     warp::serve(github).run(([0, 0, 0, 0], port)).await;
+
+    Ok(())
 }
