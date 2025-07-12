95ec02295af22b95ef64c66ef78f4172520bc2b4
Jimin Park <pjm950@gmail.com>
2025-07-13 00:53:36 +0900

    Added a dummy HTTP server.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![deny(warnings)]
+
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let port: u16 = std::env::var("PORT")
+        .ok()
+        .and_then(|p| p.parse().ok())
+        .unwrap_or(8080);
+    
+    let github = warp::get()
+        .and(warp::path("github"))
+        .map(|| "Hello, world!");
+
+    println!("Listening on 0.0.0.0:{}", port);
+    warp::serve(github).run(([0, 0, 0, 0], port)).await;
 }
